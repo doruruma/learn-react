20e99406e9981f0173e310d2e1e1c885d678fbcb
doruruma <andra.yuda0308@gmail.com>
2024-10-17 01:39:13 +0700

    formatted code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,17 @@ import { useCallback, useEffect } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // components
-import WorkoutDetails from '../components/WorkoutDetail'
-import WorkoutForm from '../components/WorkoutForm'
+import WorkoutDetails from "../components/WorkoutDetail"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-
-  const { workouts, dispatch } = useWorkoutsContext()  
+  const { workouts, dispatch } = useWorkoutsContext()
 
   const fetchWorkout = useCallback(async () => {
-    const response = await fetch('/api/workouts')
+    const response = await fetch("/api/workouts")
     const json = await response.json()
     if (response.ok) {
-      dispatch({ type: 'SET_WORKOUTS', payload: json })
+      dispatch({ type: "SET_WORKOUTS", payload: json })
     }
   }, [dispatch])
 
@@ -23,18 +22,16 @@ const Home = () => {
 
   return (
     <div className="home">
-
       <div className="workouts">
-        {workouts && workouts.map(workout => (
-          <WorkoutDetails key={workout._id} workout={workout} />
-        ))}
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
       </div>
 
       <WorkoutForm />
-
     </div>
   )
-
 }
 
 export default Home
